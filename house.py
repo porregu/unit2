c8267afa2db43a4111f0a80641d1c6054a639e93
guillermo <porregu@ssfs.org>
2019-09-18 14:39:48 -0400

    trying some variables

---
+++ house.py
@@ -0,0 +1,3 @@
+vv =(38.3+652)/60
+go_to_breakfast =8.25*2+3*7+.8
+print(vv)
---
+++ the_cover_of_a_book.py
@@ -0,0 +1,3 @@
+variable_numbbooks =60
+the_price_of_the_cover =24.95*.6*variable_numbbooks+3+.75*(variable_numbbooks-1)
+print(the_price_of_the_cover," is the price of the cover of the book")

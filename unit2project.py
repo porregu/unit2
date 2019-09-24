7fc90a87559b0aede11bb4eeafeb58af035c9345
guillermo <porregu@ssfs.org>
2019-09-24 10:55:01 -0400

    finish project 2

--- unit2project.py
+++ unit2project.py
@@ -7,12 +7,14 @@ my_number=3000/number
 print(number,"is a incredible number","my favorite number is",my_number,"times bigger than yours")
 car=input("what's your dream car")
 print(car,"wow i know that car is soo cool","my dream car is a porche")
-price=input("how much that car cost?")
+price=float(input("how much that car cost?"))
 print(price,"gee, that is spendy")
-rate=input("what will be a normal yearly interest for the car")
+rate=float(input("what will be a normal yearly interest for the car"+"%"))
 print(rate, "well that's okay")
-loan=input("how many years will take you for get out of the loan")
+loan=float(input("how many years will take you for get out of the loan"+"years"))
 print(loan,"well it's reasonable")
-mpymt=(rate*price)/(1-(1+rate)**-loan*12)
+loan=loan*12# make the loan times 12 months
+rate=rate/100/12#make the rate percent in 12 months
+mpymt=(rate*price)/(1-(1+rate)**-(loan))#monthly payment
 print("so", mpymt, "is your monthly payment")
 print("thanks a lot appreciate this, goodbye")
